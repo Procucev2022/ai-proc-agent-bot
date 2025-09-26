ddfc6b6fff9a5cbc8ec915fe115d09c77331dd67
priyasoni24 <priya.soni@bluecrestsoft.com>
2025-09-26 12:57:30 +0530

    chnaged the name

--- app/services/rfq_status_service.py
+++ app/services/rfq_status_service.py
@@ -46,7 +46,7 @@ class RFQStatusService:
             await self.whatsapp_service.send_cta_button_message(
                 recipient_id=user.phone_number,
                 body_text=response_message,
-                button_text="Click to view details",
+                button_text="View details",
                 url="https://p2pdevuiindia.azurewebsites.net/login"
             )
 
