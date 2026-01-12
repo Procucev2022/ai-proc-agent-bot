b959d9ad0b42d2916fdf62dfd0d9f39612715614
Srujan <jujare.srujan2@gmail.com>
2026-01-12 10:32:41 +0530

    BFS Notificaion update

--- app/celery_config.py
+++ app/celery_config.py
@@ -142,7 +142,7 @@ if ENABLE_TEST_CRON:
 if ENABLE_BFS_NOTIFICATION:
     beat_schedule['bfs-notification-task'] = {
         'task': 'app.tasks.bfs_notification_task.process_bfs_seller_notifications',
-        'schedule': crontab(minute='*/30'),  # Every 30 minutes
+        'schedule': crontab(minute='*/5'),  # TODO: Change to run every 30 minutes
         'options': {
             'expires': 300,
             'queue': 'bfs_notification'
